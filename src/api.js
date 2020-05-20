8c6392b10878f9cfce8379f7ab29b2c9401cf03d
Ilya Krupin <dev@krupibox.ru>
2020-05-20 14:56:15 +0300

    Add class Api

---
+++ src/api.js
@@ -0,0 +1,5 @@
+export default class Api {
+    getFilms() {
+        return fetch(`https://11.ecmascript.pages.academy/cinemaddict`);
+    }
+}
--- src/main.js
+++ src/main.js
@@ -14,15 +14,14 @@ import { generateComment } from './mock/comment';
 import { generateProfile } from './mock/profile';
 import { getRandomIntegerNumber } from './mock/utils';
 
-const films = [...Array(NumberOfFilmsToRender.TOTAL)]
-.map(() => generateFilm());
+import Api from "./api";
+
+// const films = [...Array(NumberOfFilmsToRender.TOTAL)]
+// .map(() => generateFilm());
 
 const comments = [...Array(NumberOfFilmsToRender.TOTAL)]
-.map(() => [...Array(getRandomIntegerNumber(1, 5))]
-.map(() => generateComment()));
-
-const filmsModel = new FilmsModel();
-filmsModel.setFilms(films);
+    .map(() => [...Array(getRandomIntegerNumber(1, 5))]
+        .map(() => generateComment()));
 
 const commentsModel = new CommentsModel();
 commentsModel.setComments(comments);
@@ -31,18 +30,31 @@ const siteHeader = document.querySelector(`.header`);
 const siteMain = document.querySelector(`.main`);
 const siteFooter = document.querySelector(`.footer`);
 
-render(siteHeader, new ProfileComponent(generateProfile()), RenderPosition.BEFOREEND);
+const filmsModel = new FilmsModel();
 
-const siteSection = new FilmsSectionComponent();
-const statisticComponent = new StatisticsComponent(filmsModel.getFilmsAll());
+const api = new Api();
 
-const filterController = new FilterController(siteMain, siteSection, filmsModel, statisticComponent);
-filterController.render();
+api.getFilms().then((films) => {
+    filmsModel.setFilms(films);
 
-render(siteMain, siteSection, RenderPosition.BEFOREEND);
+    render(siteHeader, new ProfileComponent(generateProfile()), RenderPosition.BEFOREEND);
 
-const pageController = new PageController(siteSection, filmsModel, commentsModel);
-pageController.render();
+    const siteSection = new FilmsSectionComponent();
+    const statisticComponent = new StatisticsComponent(filmsModel.getFilmsAll());
 
-render(siteMain, statisticComponent, RenderPosition.BEFOREEND);
-render(siteFooter, new FilmsStatisticsComponent(films), RenderPosition.BEFOREEND);
+    const filterController = new FilterController(siteMain, siteSection, filmsModel, statisticComponent);
+    filterController.render();
+
+    render(siteMain, siteSection, RenderPosition.BEFOREEND);
+
+    const pageController = new PageController(siteSection, filmsModel, commentsModel);
+    pageController.render();
+
+    render(siteMain, statisticComponent, RenderPosition.BEFOREEND);
+    render(siteFooter, new FilmsStatisticsComponent(films), RenderPosition.BEFOREEND);
+});
+
+
+
+
+
