7b818a4a963f07a3814177c3173c88d5313d3467
Ilya Krupin <dev@krupibox.ru>
2020-05-20 13:42:35 +0300

    Beutify main

--- src/main.js
+++ src/main.js
@@ -2,10 +2,10 @@ import ProfileComponent from './components/profile/profile';
 import FilmsSectionComponent from './components/films-section/films-section';
 import FilmsStatisticsComponent from './components/films-statistics/films-statistics';
 import StatisticsComponent from './components/statistic/statistic';
-import FilmsModel from "./models/films";
-import CommentsModel from "./models/comments";
 import FilterController from './controllers/filter';
 import PageController from './controllers/page';
+import FilmsModel from "./models/films";
+import CommentsModel from "./models/comments";
 import { NumberOfFilmsToRender, RenderPosition } from './consts';
 import { render } from './utils/render';
 
@@ -30,11 +30,10 @@ commentsModel.setComments(comments);
 const siteHeader = document.querySelector(`.header`);
 const siteMain = document.querySelector(`.main`);
 const siteFooter = document.querySelector(`.footer`);
-const siteSection = new FilmsSectionComponent();
-
 
 render(siteHeader, new ProfileComponent(generateProfile()), RenderPosition.BEFOREEND);
 
+const siteSection = new FilmsSectionComponent();
 const statisticComponent = new StatisticsComponent(filmsModel.getFilmsAll());
 
 const filterController = new FilterController(siteMain, siteSection, filmsModel, statisticComponent);
@@ -42,12 +41,8 @@ filterController.render();
 
 render(siteMain, siteSection, RenderPosition.BEFOREEND);
 
-const pageController = new PageController(
-    siteSection,
-    filmsModel,
-    commentsModel);
-
+const pageController = new PageController(siteSection, filmsModel, commentsModel);
 pageController.render();
-render(siteMain, statisticComponent, RenderPosition.BEFOREEND);
 
+render(siteMain, statisticComponent, RenderPosition.BEFOREEND);
 render(siteFooter, new FilmsStatisticsComponent(films), RenderPosition.BEFOREEND);
